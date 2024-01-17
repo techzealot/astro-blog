0f0b4df7a9bb430822b12c30d7bfbd3dd089d42f
techzealot <techzealot@foxmail.com>
2024-01-17 18:26:19 +0800

    部署到github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: 'https://techzealot.github.io',
+  base: '/astro-blog',
   integrations: [preact()]
 });
